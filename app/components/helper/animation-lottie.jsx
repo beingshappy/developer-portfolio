c1ceb77466be05cb74387a273794ae544ce7725c
Sahasransu Satpathy <satpathy707@gmail.com>
2025-08-05 15:49:31 +0530

    Update animation-lottie.jsx

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,6 +1,9 @@
-"use client"
+"use client";
 
-import Lottie from "lottie-react";
+import dynamic from "next/dynamic";
+
+// Dynamically import Lottie with SSR disabled
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const AnimationLottie = ({ animationPath, width }) => {
   const defaultOptions = {
@@ -8,13 +11,11 @@ const AnimationLottie = ({ animationPath, width }) => {
     autoplay: true,
     animationData: animationPath,
     style: {
-      width: '95%',
+      width: width || '95%', // fallback if width not passed
     }
   };
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
+  return <Lottie {...defaultOptions} />;
 };
 
 export default AnimationLottie;
